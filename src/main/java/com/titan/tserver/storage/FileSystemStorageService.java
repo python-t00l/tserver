c5f712758e18124afe0717debffda14d089c6aab
971946256 <971946256@qq.com>
2018-06-27 16:41:07 +0800

    修改附件文件夹的创建方式为若存在就不创建

--- src/main/java/com/titan/tserver/storage/FileSystemStorageService.java
+++ src/main/java/com/titan/tserver/storage/FileSystemStorageService.java
@@ -12,10 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URLDecoder;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 import java.util.stream.Stream;
 
 @Service
@@ -28,12 +25,11 @@ public class FileSystemStorageService implements StorageService {
     private static Logger logger = LogManager.getLogger(FileSystemStorageService.class);
 
 
-
     @Autowired
     public FileSystemStorageService(StorageProperties properties) {
         this.videoLocation = Paths.get(properties.getVideolocation());
         this.imgLocation = Paths.get(properties.getImagelocation());
-        this.rootLocation=Paths.get(properties.getRootlocation());
+        this.rootLocation = Paths.get(properties.getRootlocation());
 
     }
 
@@ -41,7 +37,7 @@ public class FileSystemStorageService implements StorageService {
     public void store(MultipartFile file) {
         String filename = file.getOriginalFilename();
         try {
-            filename = URLDecoder.decode(filename,"utf-8");
+            filename = URLDecoder.decode(filename, "utf-8");
             if (file.isEmpty()) {
                 throw new StorageException("Failed to store empty file " + filename);
             }
@@ -56,21 +52,20 @@ public class FileSystemStorageService implements StorageService {
                 case "image/*":
                     Files.copy(file.getInputStream(), this.imgLocation.resolve(filename),
                             StandardCopyOption.REPLACE_EXISTING);
-                    System.out.println("存储路径"+imgLocation);
-                    System.out.println("文件已存储"+filename);
+                    System.out.println("存储路径" + imgLocation);
+                    System.out.println("文件已存储" + filename);
                     break;
                 case "video/*":
                     Files.copy(file.getInputStream(), this.videoLocation.resolve(filename),
                             StandardCopyOption.REPLACE_EXISTING);
-                    System.out.println("存储路径"+videoLocation);
-                    logger.info("文件已存储"+filename);
-                    System.out.println("文件已存储"+filename);
+                    System.out.println("存储路径" + videoLocation);
+                    logger.info("文件已存储" + filename);
+                    System.out.println("文件已存储" + filename);
                     break;
             }
 
 
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new StorageException("Failed to store file " + filename, e);
         }
     }
@@ -81,26 +76,24 @@ public class FileSystemStorageService implements StorageService {
             return Files.walk(this.rootLocation, 1)
                     .filter(path -> !path.equals(this.rootLocation))
                     .map(this.rootLocation::relativize);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new StorageException("Failed to read stored files", e);
         }
 
     }
 
 
-
     @Override
-    public Path load(String contentType,String filename) {
-        switch (contentType){
+    public Path load(String contentType, String filename) {
+        switch (contentType) {
             case "image/*":
                 break;
             case "":
                 break;
         }
-        if(contentType.equals("image/*")){
+        if (contentType.equals("image/*")) {
             return imgLocation.resolve(filename);
-        }else {
+        } else {
             return videoLocation.resolve(filename);
 
         }
@@ -109,37 +102,40 @@ public class FileSystemStorageService implements StorageService {
     }
 
     @Override
-    public Resource loadAsResource(String contentType,String filename) {
+    public Resource loadAsResource(String contentType, String filename) {
         try {
-            Path file = load(contentType,filename);
+            Path file = load(contentType, filename);
             Resource resource = new UrlResource(file.toUri());
             if (resource.exists() || resource.isReadable()) {
                 return resource;
-            }
-            else {
+            } else {
                 throw new StorageFileNotFoundException(
                         "Could not read file: " + filename);
 
             }
-        }
-        catch (MalformedURLException e) {
+        } catch (MalformedURLException e) {
             throw new StorageFileNotFoundException("Could not read file: " + filename, e);
         }
     }
 
     @Override
     public void deleteAll() {
-        FileSystemUtils.deleteRecursively(rootLocation.toFile());
+//        FileSystemUtils.deleteRecursively(rootLocation.toFile());
     }
 
     @Override
     public void init() {
         try {
-            //Files.createDirectories(rootLocation);
-            Files.createDirectories(videoLocation);
-            Files.createDirectories(imgLocation);
-        }
-        catch (IOException e) {
+//            //Files.createDirectories(rootLocation);
+            if (!Files.exists(videoLocation)) {
+                Files.createDirectories(videoLocation);
+            }
+            if (!Files.exists(imgLocation)) {
+                Files.createDirectories(imgLocation);
+            }
+        }catch (FileAlreadyExistsException e){
+            System.out.println("文件已存在");
+        } catch (IOException e) {
             throw new StorageException("Could not initialize storage", e);
         }
     }
